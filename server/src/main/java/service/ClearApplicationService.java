a09f0371a654fa3be2b7bdc3a2c45f5162868f2e
Thomas Bean <47thomasj@gmail.com>
2026-02-16 16:39:07 -0700

    clear all first draft

--- server/src/main/java/service/ClearApplicationService.java
+++ server/src/main/java/service/ClearApplicationService.java
@@ -1,5 +1,26 @@
 package service;
 
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
+import dataaccess.UserDAO;
+import models.requests.ClearAllRequest;
+import models.results.ClearAllResult;
+
 public class ClearApplicationService {
-    
+    private final AuthDAO authDAO;
+    private final GameDAO gameDAO;
+    private final UserDAO userDAO;
+    public ClearApplicationService(AuthDAO authDAO, GameDAO gameDAO, UserDAO userDAO) {
+        this.authDAO = authDAO;
+        this.gameDAO = gameDAO;
+        this.userDAO = userDAO;
+    }
+
+    public ClearAllResult clear(ClearAllRequest request) {
+        authDAO.clear();
+        gameDAO.clear();
+        userDAO.clear();
+
+        return new ClearAllResult(true);
+    }
 }
