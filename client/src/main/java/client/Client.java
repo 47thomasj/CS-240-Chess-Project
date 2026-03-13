afce3553ef5a65c94eb9507fc91f7024d84cf0b1
Thomas Bean <47thomasj@gmail.com>
2026-03-12 18:18:57 -0600

    Client first draft

--- client/src/main/java/client/Client.java
+++ client/src/main/java/client/Client.java
@@ -1,6 +1,7 @@
 package client;
 
 import menu.Menu;
+import menu.MenuOption;
 
 public class Client {
     
@@ -8,13 +9,59 @@ public class Client {
     private final Menu postlogin;
 
     public Client() {
-        this.prelogin = new Menu();
-        this.postlogin = new Menu();
+        String preLoginHelpString = "This is the landing page menu. You can select an option by entering the number of the option."
+        + "\n\nOptions:"
+        + "\n1. Login with your username and password"
+        + "\n2. Register a new account"
+        + "\n3. Quit";
+        
+        String postLoginHelpString = "This is the main menu. You can select an option by entering the number of the option."
+        + "\n\nOptions:"
+        + "\n1. Logout of your account"
+        + "\n2. Create a new Chess Game"
+        + "\n3. List all Chess Games available"
+        + "\n4. Join and begin playing a pre-existing Chess Game"
+        + "\n5. Observe a pre-existing Chess Game, but not participate in it"
+        + "\n6. Quit";
+
+        this.prelogin = new Menu("Quit", preLoginHelpString, "Welcome to the Chess Game!");
+        this.postlogin = new Menu("Logout", postLoginHelpString, "You are now logged in. Welcome!");
+
+        this.prelogin.addOption(new MenuOption("Login", () -> {
+            System.out.println("Login with your username and password");
+            this.postlogin.ineteractWithMenu();
+        }));
+        this.prelogin.addOption(new MenuOption("Register", () -> {
+            System.out.println("Register a new account");
+        }));
+        this.prelogin.addOption(new MenuOption("Quit", () -> {
+            System.out.println("Quit");
+        }));
+
+
+        this.postlogin.addOption(new MenuOption("Logout", () -> {
+            System.out.println("Logout of your account");
+        }));
+        this.postlogin.addOption(new MenuOption("Create a new Chess Game", () -> {
+            System.out.println("Create a new Chess Game");
+        }));
+        this.postlogin.addOption(new MenuOption("List all Chess Games available", () -> {
+            System.out.println("List all Chess Games available");
+        }));
+        this.postlogin.addOption(new MenuOption("Join and begin playing a pre-existing Chess Game", () -> {
+            System.out.println("Join and begin playing a pre-existing Chess Game");
+        }));
+        this.postlogin.addOption(new MenuOption("Observe a pre-existing Chess Game, but not participate in it", () -> {
+            System.out.println("Observe a pre-existing Chess Game, but not participate in it");
+        }));
+        this.postlogin.addOption(new MenuOption("Quit", () -> {
+            System.out.println("Quit");
+        }));
     }
 
     public void run() {
         try {
-            
+            this.prelogin.ineteractWithMenu();
         } catch (Exception e) {
         }
     }
