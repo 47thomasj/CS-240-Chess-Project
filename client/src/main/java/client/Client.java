d76bbba8bb3d6173fb3d7e372e06924e77b47849
Thomas Bean <47thomasj@gmail.com>
2026-03-12 18:09:15 -0600

    Started client then gave up

---
+++ client/src/main/java/client/Client.java
@@ -0,0 +1,21 @@
+package client;
+
+import menu.Menu;
+
+public class Client {
+    
+    private final Menu prelogin;
+    private final Menu postlogin;
+
+    public Client() {
+        this.prelogin = new Menu();
+        this.postlogin = new Menu();
+    }
+
+    public void run() {
+        try {
+            
+        } catch (Exception e) {
+        }
+    }
+}
