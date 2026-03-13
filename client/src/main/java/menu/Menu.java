9e8ab6c1e75ab99023321e52c7f80f031013b11d
Thomas Bean <47thomasj@gmail.com>
2026-03-12 18:09:02 -0600

    Menu First Draft

---
+++ client/src/main/java/menu/Menu.java
@@ -0,0 +1,52 @@
+package menu;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Menu {
+    
+    private ArrayList<MenuOption> options;
+    private String abortOption;
+    private String helpString;
+    private String titleString;
+
+    public Menu(String abortOption, String helpString, String titleString) {
+        this.options = new ArrayList<>();
+        this.abortOption = abortOption;
+        this.helpString = helpString;
+        this.titleString = titleString;
+    }
+
+    public void addOption(MenuOption option) {
+        options.add(option);
+    }
+
+    public void printMenu() {
+        System.out.println(titleString);
+        System.out.println("Options:");
+        for (int i = 0; i < options.size(); i++) {
+            System.out.println(i + 1 + ". " + options.get(i).toString());
+        }
+        System.out.println(options.size() + 1 + ". " + abortOption);
+        System.out.println(options.size() + 2 + ". Help");
+    }
+
+    public void ineteractWithMenu() {
+        printMenu();
+        try (Scanner scanner = new Scanner(System.in)) {
+            int choice = scanner.nextInt();
+            if (choice == options.size() + 1) {
+                return;
+            }
+            if (choice == options.size() + 2) {
+                System.out.println(helpString);
+            }
+            options.get(choice - 1).execute();
+        }
+        catch (Exception e) {
+            System.out.println("Please select a valid menu option. (1-" + (options.size() + 2) + ")");
+            ineteractWithMenu();
+        }
+    }
+
+}
