d892c4800a40c1a0e4a3febcb228d3b21e0ac912
Thomas Bean <47thomasj@gmail.com>
2026-03-16 17:57:55 -0600

    fixed chess printer

--- client/src/main/java/board/ChessPrinter.java
+++ client/src/main/java/board/ChessPrinter.java
@@ -10,42 +10,64 @@ public class ChessPrinter {
     public static void printBoard(ChessBoard board, TeamColor teamColor) {
     String letters = teamColor == TeamColor.WHITE ? " a  b  c  d  e  f  g  h " : " h  g  f  e  d  c  b  a ";
 
-    String[] numbersWhite = new String[] {"1", "2", "3", "4", "5", "6", "7", "8"};
-    String[] numbersBlack = new String[] {"8", "7", "6", "5", "4", "3", "2", "1"};
-    String[] numbers = teamColor == TeamColor.WHITE ? numbersWhite : numbersBlack;
-    String topLeftCornerColor = teamColor == TeamColor.WHITE ? EscapeSequences.SET_BG_COLOR_WHITE : EscapeSequences.SET_BG_COLOR_BLACK;
+    String[] numbers = new String[] {"1", "2", "3", "4", "5", "6", "7", "8"};
+    String topLeftCornerColor = EscapeSequences.SET_BG_COLOR_WHITE;
     String teamTextColor = teamColor == TeamColor.WHITE ? EscapeSequences.SET_TEXT_COLOR_BLUE : EscapeSequences.SET_TEXT_COLOR_RED;
     String oponentTextColor = teamColor == TeamColor.WHITE ? EscapeSequences.SET_TEXT_COLOR_RED : EscapeSequences.SET_TEXT_COLOR_BLUE;
 
     String currentColor = topLeftCornerColor;
     System.out.println("\n");
     System.out.println(EscapeSequences.SET_BG_COLOR_LIGHT_GREY + "   " + letters + "   " + EscapeSequences.RESET_BG_COLOR);
-    for (int row = 8; row >= 1; row--) {
+    if (teamColor == TeamColor.WHITE) {
+        for (int row = 8; row >= 1; row--) {
+            printWholeRow(board, numbers, row, currentColor, teamTextColor, oponentTextColor);
+            currentColor = switchColor(currentColor);
+        }
+    } else {
+        for (int row = 1; row <= 8; row++) {
+            printWholeRow(board, numbers, row, currentColor, teamTextColor, oponentTextColor);
+            currentColor = switchColor(currentColor);
+        }
+    }
+    System.out.println(EscapeSequences.SET_BG_COLOR_LIGHT_GREY + "   " + letters + "   " + EscapeSequences.RESET_BG_COLOR);
+    }
+
+    private static void printWholeRow(ChessBoard board, String[] numbers, int row, String currentColor, String teamTextColor, String oponentTextColor) {
         System.out.print(EscapeSequences.SET_BG_COLOR_LIGHT_GREY + " " + numbers[row - 1] + " " + EscapeSequences.RESET_BG_COLOR);
         System.out.print(printRow(board, row, currentColor, teamTextColor, oponentTextColor));
         System.out.print(EscapeSequences.SET_BG_COLOR_LIGHT_GREY + " " + numbers[row - 1] + " " + EscapeSequences.RESET_BG_COLOR);
         System.out.println();
-        currentColor = switchColor(currentColor);
-    }
-    System.out.println(EscapeSequences.SET_BG_COLOR_LIGHT_GREY + "   " + letters + "   " + EscapeSequences.RESET_BG_COLOR);
     }
 
     private static String printRow(ChessBoard board, int row, String startingColor, String teamTextColor, String oponentTextColor) {
         String currentColor = startingColor;        
         String rowString = "";
 
-        for (int i = 1; i <= 8; i++) {
-            ChessPosition position = new ChessPosition(row, i);
-            ChessPiece pieceAtPosition = board.getPiece(position);
-
-            TeamColor colorAtPosition = pieceAtPosition != null ? pieceAtPosition.getTeamColor() : null;
-            String textColor = colorAtPosition == TeamColor.WHITE ? teamTextColor : oponentTextColor;
-            rowString += currentColor + textColor + " ";
-            rowString += pieceAtPosition != null ? pieceAtPosition.toString() : " "; 
-            rowString += " ";
-            currentColor = switchColor(currentColor);
+        if (teamTextColor.equals(EscapeSequences.SET_TEXT_COLOR_BLUE)) {
+            for (int i = 1; i <= 8; i++) {
+                rowString += printPiece(row, i, board, teamTextColor, oponentTextColor, currentColor);
+                currentColor = switchColor(currentColor);
+            }
+        } else {
+            for (int i = 8; i >= 1; i--) {
+                rowString += printPiece(row, i, board, teamTextColor, oponentTextColor, currentColor);
+                currentColor = switchColor(currentColor);
+            }
         }
         rowString += EscapeSequences.RESET_TEXT_COLOR + EscapeSequences.RESET_BG_COLOR;
+        return rowString;
+    }
+
+    private static String printPiece(int row, int col, ChessBoard board, String teamTextColor, String oponentTextColor, String currentColor) {
+        String rowString = "";
+        ChessPosition position = new ChessPosition(row, col);
+        ChessPiece pieceAtPosition = board.getPiece(position);
+
+        TeamColor colorAtPosition = pieceAtPosition != null ? pieceAtPosition.getTeamColor() : null;
+        String textColor = colorAtPosition == TeamColor.WHITE ? teamTextColor : oponentTextColor;
+        rowString += currentColor + textColor + " ";
+        rowString += pieceAtPosition != null ? pieceAtPosition.toString() : " "; 
+        rowString += " ";
         return rowString;
     }
 
