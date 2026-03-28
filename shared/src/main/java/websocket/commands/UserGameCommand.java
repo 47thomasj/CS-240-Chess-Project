20be6d5825fd1eab33032e6a5616eb6415dfe494
Thomas Bean <47thomasj@gmail.com>
2026-03-28 17:33:51 -0600

    added move to user game command

--- shared/src/main/java/websocket/commands/UserGameCommand.java
+++ shared/src/main/java/websocket/commands/UserGameCommand.java
@@ -2,6 +2,8 @@ package websocket.commands;
 
 import java.util.Objects;
 
+import chess.ChessMove;
+
 /**
  * Represents a command a user can send the server over a websocket
  * <p>
@@ -16,10 +18,17 @@ public class UserGameCommand {
 
     private final Integer gameID;
 
+    private final ChessMove move;
+
     public UserGameCommand(CommandType commandType, String authToken, Integer gameID) {
+        this(commandType, authToken, gameID, null);
+    }
+    
+    public UserGameCommand(CommandType commandType, String authToken, Integer gameID, ChessMove move) {
         this.commandType = commandType;
         this.authToken = authToken;
         this.gameID = gameID;
+        this.move = move;
     }
 
     public enum CommandType {
@@ -41,6 +50,10 @@ public class UserGameCommand {
         return gameID;
     }
 
+    public ChessMove getMove() {
+        return move;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -51,11 +64,12 @@ public class UserGameCommand {
         }
         return getCommandType() == that.getCommandType() &&
                 Objects.equals(getAuthToken(), that.getAuthToken()) &&
-                Objects.equals(getGameID(), that.getGameID());
+                Objects.equals(getGameID(), that.getGameID()) &&
+                Objects.equals(getMove(), that.getMove());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getCommandType(), getAuthToken(), getGameID());
+        return Objects.hash(getCommandType(), getAuthToken(), getGameID(), getMove());
     }
 }
